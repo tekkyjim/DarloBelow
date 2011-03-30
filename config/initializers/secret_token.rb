7d7d523bfe643b224fa5008ae164ffd2b92953a4
Tekkyjim <tekkyjim@gmail.com>
2011-03-30 11:57:49 +0100

    starting out.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Darlo::Application.config.secret_token = '03ce4c5a5b5318a487f50bffd4ab9f00685df9320d9961720960a028d2d1fe3226eacc4c968030d07dfeb425eca9911e5121f7dc844116fc79d8d1288a81de8e'
