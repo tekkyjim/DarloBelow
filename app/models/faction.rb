691d2638ba53b1394a3c9d703125f2621921f292
Tekkyjim <tekkyjim@gmail.com>
2011-04-02 23:23:48 +0100

    added faction button, starting on showing chars to only needed peeps

--- app/models/faction.rb
+++ app/models/faction.rb
@@ -3,6 +3,7 @@ class Faction
   field :name, :type => String
   field :description, :type => String
   field :greeting, :type => String
+  field :long_description, :type => String
   references_many :characters, :inverse_of => :characters
 
 end
