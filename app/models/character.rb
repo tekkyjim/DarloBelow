5bcb50c9895694753eeb2c8c25a6cedce4c7588d
James Mitchell <tekkyjim@gmail.com>
2011-05-09 09:41:42 +0100

    [fixes #13146489] helpers should be able to make npcs

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,15 +4,15 @@ class Character
   include Mongoid::Versioning
   include Mongoid::Timestamps
   include Mongoid::Taggable
+  max_versions 5
   field :name, :type => String
   field :description, :type => String
   field :long_description, :type => String
   field :npc, :type => Boolean, :default => false
   field :gender
   field :roleplay_hints
+  field :max_adjectives, :type => Integer, :default => 5
   validates_presence_of :name
   referenced_in :user
   referenced_in :faction
-  max_versions 5
-  field :max_adjectives, :type => Integer, :default => 5
 end
